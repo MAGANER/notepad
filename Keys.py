dae52d1f6bc2dfaa0b3bf51aa259d4ee8051caf9
MAGANER <maganer.krik@mail.ru>
2021-01-15 22:18:29 +0300

    contains hash table with keys

---
+++ Keys.py
@@ -0,0 +1,12 @@
+#module contains all keys
+#so it can be changed
+
+#hash table key is name of function
+#value is name of button
+Keys = {
+    "quit":  "esc",
+    "movef": "ctrl+f", #move forward
+    "moveb": "ctrl+b", #move back
+    "moven": "ctrl+n", #move to the new line
+    "movep": "ctrl+p"  #move to the previous line
+       }
