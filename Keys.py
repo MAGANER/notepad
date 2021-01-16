221d51f7ddc3f703e8a2240c659e1f8abb88f957
MAGANER <maganer.krik@mail.ru>
2021-01-16 13:31:54 +0300

    added key to move to custom line

--- Keys.py
+++ Keys.py
@@ -11,5 +11,6 @@ Keys = {
     "movep": "ctrl+p", #move to the previous line
     "open":  "ctrl+o",
     "scrollup": "alt+u",
-    "scrolld":  "alt+d" #scroll text down
+    "scrolld":  "alt+d", #scroll text down
+    "goto":  "ctrl+g" #go to specific line
        }
